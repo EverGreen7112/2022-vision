e66688c89943c2786209209da993b8aba4d1514b
senpairo <nadavh2611@gmail.com>
2022-10-10 14:47:03 +0300

    added calibration and backup code for limelight

---
+++ calib_limelight/limelight_code_backup.py
@@ -0,0 +1,64 @@
+import math
+import socket
+import struct
+import gbvision as gbv
+import cv2 
+import numpy as np
+import settings as settings
+from track_object import track_object
+
+cam = gbv.USBCamera(settings.CAMERA_PORT, gbv.CameraData(777.3291449774972,1.0402162342 , 0.86742863824, name="limelight"))
+blue_obj = track_object(cam=cam, pid_vals=[110, 253, 154], hue=[settings.HUE_KP, settings.HUE_KI,
+                        settings.HUE_KD], sat=[settings.SAT_KP, settings.SAT_KI,
+                        settings.SAT_KD], val=[settings.VAL_KP, settings.VAL_KI, settings.VAL_KD],
+                        range=[5, 40, 60], target= gbv.GameObject(0.212694462109))
+red_obj = track_object(cam=cam, pid_vals=[0, 0, 0], hue=[settings.HUE_KP, settings.HUE_KI,
+                        settings.HUE_KD], sat=[settings.SAT_KP, settings.SAT_KI,
+                        settings.SAT_KD], val=[settings.VAL_KP, settings.VAL_KI, settings.VAL_KD],
+                        range=[5, 40, 60], target=gbv.GameObject(0.212694462109))
+hub_obj = track_object(cam=cam, pid_vals=[94, 118, 202], hue=[settings.HUE_KP, settings.HUE_KI,
+                        settings.HUE_KD], sat=[settings.SAT_KP, settings.SAT_KI,
+                        settings.SAT_KD], val=[settings.VAL_KP, settings.VAL_KI, settings.VAL_KD],
+                        range=[10, 110, 90], target= gbv.GameObject(0.13490737563232041))
+balls_port = 5801
+hub_port = 5800
+balls_mode = 0
+hub_mode = 1
+def runPipeline(image, llrobot):
+    global blue_obj
+    global red_obj 
+    global hub_obj
+    global balls_port
+    global hub_port
+    global cam
+    global balls_mode
+    global hub_mode
+    mode = hub_mode
+    cam.width = 960
+    cam.height = 720
+    port = hub_port
+    obj = hub_obj
+    obj.cam = cam
+    obj.track_cycle(image, mode)
+
+    frame = image
+    largestContour = ()
+    if mode == hub_mode:
+        frame = gbv.draw_rotated_rects(
+            image, obj.get_rects(), (255, 0, 0), thickness=5)
+    elif mode == balls_mode:
+        frame = gbv.draw_circles(
+            image, obj.get_circs(), (255, 0, 0), thickness=5)
+    try:
+        frame2 = gbv.draw_rotated_rects(frame, [obj.get_bbox()], (0, 0, 255), thickness=5)
+        frame = frame2
+    except:
+        pass
+
+    locals = obj.get_locals()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.sendto(struct.pack('fff', locals[0], locals[1], locals[2]),
+                    ("255.255.255.255", port))
+    return largestContour, frame, locals
---
+++ calib_limelight/limelight_median_thr.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import gbvision as gbv
+stdv = np.array([5, 40, 60])
+# stdv = np.array([0, 0, 0])
+
+# basically this whole piece of code is here to write the HSV values to thr.txt
+
+
+def main():
+    # camera = gbv.USBCamera(settings.CAMERA_PORT, gbv.CameraData(23.65066003307307,1.0402162342 , 0.86742863824, name="limelight"))
+    # camera.set_exposure(settings.EXPOSURE)
+    # window = gbv.CameraWindow('feed', camera)
+    window = gbv.FeedWindow("feed")
+    window.open()
+    img = cv2.imread('thr.jpg')
+    while True:
+        # frame = window.show_and_get_frame()
+        # ok, frame = camera.read()
+        window.show_frame(img)
+        frame = img
+
+        k = window.last_key_pressed
+        if k == 'r':
+            bbox = cv2.selectROI('feed', frame)
+            
+            thr = gbv.median_threshold(frame, stdv, bbox, 'HSV')
+            print(thr)
+            break
+    cv2.destroyAllWindows()
+
+    original = gbv.FeedWindow(window_name='original')
+    after_proc = gbv.FeedWindow(window_name='after threshold', drawing_pipeline=thr)
+
+    original.open()
+    after_proc.open()
+    while True:
+        # ok, frame = camera.read()
+        frame = img
+        if not original.show_frame(frame):
+            break
+        if not after_proc.show_frame(frame):
+            break
+
+    exit()
+
+
+if __name__ == '__main__':
+    main()
