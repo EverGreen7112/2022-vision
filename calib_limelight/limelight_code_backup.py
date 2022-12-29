31f4f773b2a135bb81f01e51e9c7cd7ac45633b1
itayst1 <itay.steinberg2007@gmail.com>
2022-12-29 19:54:20 +0200

    fixed vision fr
    probably

--- calib_limelight/limelight_code_backup.py
+++ calib_limelight/limelight_code_backup.py
@@ -6,11 +6,11 @@ import cv2
 import numpy as np
 import settings as settings
 from track_object import track_object
-cam_angle = 48
+cam_angle = 37
 denoise_hub = gbv.MedianBlur(3) + gbv.Dilate(5, 2) + gbv.Erode(5, 2) + gbv.DistanceTransformThreshold(0.166)
 denoise_red = gbv.MedianBlur(3) + gbv.Dilate(5, 2) + gbv.Erode(5, 2) + gbv.DistanceTransformThreshold(0.1)
 denoise_blue = gbv.MedianBlur(3) + gbv.Dilate(5, 2) + gbv.Erode(5, 2) + gbv.DistanceTransformThreshold(0.1)
-cam = gbv.USBCamera(settings.CAMERA_PORT, gbv.CameraData(777.3291449774972,1.0402162342 , 0.86742863824, pitch_angle=math.radians(cam_angle), name="limelight"))
+cam = gbv.USBCamera(settings.CAMERA_PORT, gbv.CameraData(777.3291449774972,1.0402162342 , 0.86742863824, pitch_angle=math.radians(0), name="limelight"))
 blue_obj = track_object(cam=cam, pid_vals=[110, 253, 154],
                         hue=[settings.HUE_KP, settings.HUE_KI, settings.HUE_KD], sat=[settings.SAT_KP, settings.SAT_KI, settings.SAT_KD], val=[settings.VAL_KP, settings.VAL_KI, settings.VAL_KD],
                         range=[5, 40, 60], target= gbv.GameObject(0.212694462109), denoise_pipe = denoise_blue)
@@ -19,7 +19,7 @@ red_obj = track_object(cam=cam, pid_vals=[0, 255, 142],
                         range=[5, 40, 60], target=gbv.GameObject(0.212694462109), denoise_pipe = denoise_red)
 hub_obj = track_object(cam=cam, pid_vals=[94, 213, 216],
                         hue=[settings.HUE_KP, settings.HUE_KI, settings.HUE_KD], sat=[settings.SAT_KP, settings.SAT_KI, settings.SAT_KD], val=[settings.VAL_KP, settings.VAL_KI, settings.VAL_KD],
-                        range=[10, 110, 90], target= gbv.GameObject(0.08868483523128404), denoise_pipe = denoise_hub)
+                        range=[10, 110, 90], target= gbv.GameObject(0.07362744053679986), denoise_pipe = denoise_hub)
 
 def runPipeline(image, llrobot):
     global blue_obj
@@ -51,9 +51,9 @@ def runPipeline(image, llrobot):
 
     locals = obj.get_locals()
     locals[1] = locals[1] * 72 / 96
-    # distance = math.sqrt(locals[1]**2+locals[2]**2)
-    # locals[2] = math.cos(math.radians(cam_angle))*distance
-    # locals[1] = math.sin(math.radians(cam_angle))*distance
+    distance = math.sqrt(locals[1]**2+locals[2]**2)
+    locals[2] = math.cos(math.radians(cam_angle))*distance
+    locals[1] = math.sin(math.radians(cam_angle))*distance
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
